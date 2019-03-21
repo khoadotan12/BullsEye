3ec1a9827f78550e7233632d10edfbcf674b968b
Đỗ Tấn Khoa <khoadt2@vng.com.vn>
2019-03-21 22:43:58 +0700

    create about author view controller

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Đỗ Tấn Khoa on 3/21/19.
+//  Copyright © 2019 Đỗ Tấn Khoa. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
